66d0c89bbd9c0dcf546a683bfddfad6fc30771ec
Dave Galvin <david.galvin@gmail.com>
2021-01-03 02:20:04 -0500

    adding testing todos.

--- test/unit/test_logic_grid_puzzle.gd
+++ test/unit/test_logic_grid_puzzle.gd
@@ -29,5 +29,13 @@ class TestLogicGridPuzzle:
 		#_lp.eliminate_possible_solutions(2,1,1,2,true)
 #		_lp.eliminate_possible_solutions(1,3,0,0,true)
 
-
 		print(_lp)
+# TODO: Write some real lp tests:
+# test set one: moves that should imply something about the puzzle.
+
+# test set two: create a programmatic tester looking for failures to discover
+# all implied information. Randomly generate puzzles with category count &
+# size restrictions, and enter random legal moves. If the puzzle becomes
+# unsolvable, then we missed some implied information. Record moves for analysis.
+# bonus version: on finding an invalid puzzle, rerun the moves with elimination
+# to find the minimum move set with the error.
